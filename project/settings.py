86ae1c921b270da82e98b30c353b99786f5d5e68
Sujan Adhikari <sujanadhikari.com@gmail.com>
2022-05-25 19:25:02 +0545

    finishing heroku development

--- project/settings.py
+++ project/settings.py
@@ -21,13 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ["SECRET_KEY"]
-SECRET_KEY = 'django-insecure-u26g3imy6lyqvj^3x@*ol%20-^3^$^j0($pp($v1!a0b@#%idr'
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["uploaddjangoimg.herokuapp.com",'localhost']
 
 
 # Application definition
