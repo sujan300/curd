3e266a45ca8ca35fb63f314a7b181d14ae8b3af1
Sujan Adhikari <sujanadhikari.com@gmail.com>
2022-05-25 18:20:49 +0545

    finishing heroku development

--- project/settings.py
+++ project/settings.py
@@ -122,9 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [ 
-    os.path.join(BASE_DIR, "static"),
-]
+# STATICFILES_DIRS = [ 
+#     os.path.join(BASE_DIR, "static"),
+# ]
 
 # for upload photoes in our project ..
 
