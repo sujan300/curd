fe30f4f2b1e498a51f0a695d66ac0fc2f2a8bfa6
Sujan Adhikari <sujanadhikari.com@gmail.com>
2022-05-25 18:12:19 +0545

    finishing heroku development 22

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = os.environ["SECRET_KEY"]
 SECRET_KEY = 'django-insecure-u26g3imy6lyqvj^3x@*ol%20-^3^$^j0($pp($v1!a0b@#%idr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["uploaddjangoimg.herokuapp.com",'localhost']
 
